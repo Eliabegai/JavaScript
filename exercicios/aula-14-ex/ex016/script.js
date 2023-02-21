1b5278b47728c587a6252d5e88dd80fdce0bd212
Eliabe Gai <101521522+Eliabegai@users.noreply.github.com>
2023-02-21 18:54:37 -0300

    melhoria no exercício de contar

--- exercicios/aula-14-ex/ex016/script.js
+++ exercicios/aula-14-ex/ex016/script.js
@@ -5,25 +5,36 @@ function contar() {
   var passo = parseInt(document.getElementById('inputPasso').value)
   var res = document.querySelector('div#res')
 
-
   if (isNaN(passo) || passo <= 0){
     window.alert('O valor do passo deve ser maior que zero')
-  } else {
-    if (isNaN(inicio) || isNaN(final)) {
-      res.innerHTML = 'Não pode ser contado!'
+  } else if (isNaN(inicio) || isNaN(final)) {
+      res.innerHTML = '\u{1F4A5} Não pode ser contado! \u{1F4A5}	'
+      console.log(res)
       res.style.color = 'red'
-    } else {
+  } else {
+    // contagem crescente
+    if (inicio < final) {
       var text = ''
       for (var c = inicio; c <= final; c += passo) {
         if (c <= (final - passo)){
           text += `${c} → `
         } else {
-          text += `${c} !`
+          text += `${c} \u{1F606}`
         }
       }
-      res.innerHTML = `<strong>Resultado:</strong> ${text}`
-      res.style.color = 'green'
+    } else {
+    // contagem decrescente 
+      var text = ''
+      for (var c = inicio; c >= final; c -= passo) {
+        if (c >= (final + passo)){
+          text += `${c} → `
+        } else {
+          text += `${c} \u{1F606}`
+        }
+      }
     }
+    res.innerHTML = `<strong>Resultado:</strong> ${text}`
+    res.style.color = 'green'
   }
-
+    
 }
