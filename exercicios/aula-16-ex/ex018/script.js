f1ffe5f655e98c24e02293c3729cbec00abed034
Eliabe Gai <101521522+Eliabegai@users.noreply.github.com>
2023-02-23 19:41:17 -0300

    conclusão do exercício do Analisador de Números
    Finalizado, com as condições desejadas como se o valor for inválido, não segue para frente ou caso já tenha

--- exercicios/aula-16-ex/ex018/script.js
+++ exercicios/aula-16-ex/ex018/script.js
@@ -5,28 +5,42 @@ function adicionar() {
   let number = parseInt(document.getElementById('inputNumber').value)
   let cadastro = document.getElementById('cadastro')
   
-if (number > 1 && number <=100){
-  if (addNumber.length === 0) {
-    cadastro.innerHTML = ''
+  if (res.length !== 0) {
+    res.innerHTML = ''
   }
-    
-    addNumber.push(number)
-    
-    let item = document.createElement('option')
-    item.text += `Valor ${number} adicionado!`
-    item.value += `tab${number}`
-    cadastro.appendChild(item)
+
+  if (number >= 1 && number <=100){
+    if (addNumber.length === 0) {
+      cadastro.innerHTML = ''
+    }
+    if (addNumber.indexOf(number) >= 0 ){
+      window.alert('O número já está cadastrado')
+    } else {
+      addNumber.push(number)
+      let item = document.createElement('option')
+      item.text += `Valor ${number} adicionado!`
+      item.value += `tab${number}`
+      cadastro.appendChild(item)
+      document.getElementById('inputNumber').value = ''
+    }
   } else {
     window.alert('[ERROR] Valor Inválido!')
   }
 }
 
-
 function finalizar() {
   if (addNumber.length !== 0){
-    res.innerHTML = 'Finalizado'
+    let soma = 0
+    res.innerHTML = `<p>Ao todo, temos ${addNumber.length} cadastrados.</p>`
+    res.innerHTML += `<p>O maior valor informado é:  ${Math.max(...addNumber)}.</p>`
+    res.innerHTML += `<p>O menor valor informado é: ${Math.min(...addNumber)}.</p>`
+
+    for(let i = 0; i < addNumber.length; i++){
+      soma += addNumber[i]
+    }
+    res.innerHTML += `<p>Somando todos os valores, temos: ${soma}.</p>`
+    res.innerHTML += `<p>A média dos valores digitados é: ${soma / addNumber.length}.</p>`
   } else {
     window.alert('[ERROR] Adicione valor para Finalizar!')
   }
-
 }
