78c611bf2de6ef8de05a192e38c9307fea726e12
Eliabe Gai <101521522+Eliabegai@users.noreply.github.com>
2023-02-23 22:12:52 -0300

    Desafio 002 - Concluido

---
+++ desafios/d002/script.js
@@ -0,0 +1,5 @@
+function startDesafio() {
+  let name = window.prompt('Qual é o seu nome? ')
+  let ano = window.prompt(`Olá ${name}, quantos anos você tem? `)
+  window.alert(`Prazer em te conhecer ${name} que tem ${ano} anos de idade!`)
+}
