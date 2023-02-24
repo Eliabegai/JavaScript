e91ef5516a8ee0ee48f76cd25ef130bb2be83a07
Eliabe Gai <101521522+Eliabegai@users.noreply.github.com>
2023-02-23 22:25:25 -0300

    Desafio 003 - Concluido

---
+++ desafios/d003/script.js
@@ -0,0 +1,7 @@
+function analisar() {
+    let num = parseInt(window.prompt('Digite um número inteiro qualquer!'))
+    window.alert(`
+    Antes de ${num}, temos o número ${num - 1}. 
+    Depois de ${num}, temos o número ${num + 1}.
+    `)
+  }
