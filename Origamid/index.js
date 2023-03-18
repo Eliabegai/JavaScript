d031c34a6bb1a0e72dbb1df3d8e9d748f73a3544
Eliabe Gai <101521522+Eliabegai@users.noreply.github.com>
2023-03-18 01:22:13 -0300

    Update index.js

--- Origamid/index.js
+++ Origamid/index.js
@@ -10,6 +10,12 @@ res.innerHTML += `<p>${nome.length}</p>`
 res.innerHTML += `<p>${nome.charAt(1)}</p>`
 res.innerHTML += `<p>${nome.replace('Eliabe', 'Ebaile')}</p>`
 
+var altura = 1.8
+res.innerHTML += `<h3>Números</h3>`
+res.innerHTML += `<p>${altura.toString()}</p>`
+res.innerHTML += `<p>${altura.toFixed()}</p>`
+
+res.innerHTML += `<h3>Elementos DOM</h3>`
 
 const esconder = function() {
   res.innerHTML = ''
