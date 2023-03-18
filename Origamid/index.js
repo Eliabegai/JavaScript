51bdc3fe181743e23ab3a759e50c426dd28b83aa
Eliabe Gai <101521522+Eliabegai@users.noreply.github.com>
2023-03-17 21:14:01 -0300

    Update index.js

--- Origamid/index.js
+++ Origamid/index.js
@@ -5,3 +5,19 @@ const esconder = function() {
 }
 
 const btn = document.querySelector('button')
+
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+
