ea22008fef0bd998f3ae7b595996ef7adda4d204
Eliabe Gai <101521522+Eliabegai@users.noreply.github.com>
2023-03-18 01:15:53 -0300

    Update index.js

--- Origamid/index.js
+++ Origamid/index.js
@@ -3,7 +3,12 @@ const btn = document.getElementById('esconde')
 const container = document.querySelector('div#container')
 
 
+var nome = 'Eliabe'
 
+res.innerHTML = `<h3>${nome}</h3>`
+res.innerHTML += `<p>${nome.length}</p>`
+res.innerHTML += `<p>${nome.charAt(1)}</p>`
+res.innerHTML += `<p>${nome.replace('Eliabe', 'Ebaile')}</p>`
 
 
 const esconder = function() {
